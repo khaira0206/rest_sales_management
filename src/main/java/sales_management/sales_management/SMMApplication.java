e67cf64eadd9d2ab050ae4b2c613be8b8000b88a
harmanjot singh <khaira0206@gmail.com>
2019-08-24 15:22:47 -0400

    first commit

---
+++ src/main/java/sales_management/sales_management/SMMApplication.java
@@ -0,0 +1,10 @@
+package sales_management.sales_management;
+
+
+public class SMMApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
